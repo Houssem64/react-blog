ef541b2e2350ecb7696f39c751263d9245bd3939
Houssem Mehouachi <fazeputin@yandex.com>
2024-03-30 11:27:29 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import ReactDOM from "react";
 import Home from "./pages/Home";
-import Article from "./pages/Article";
+import Articles from "./pages/Article";
 import { Analytics } from "@vercel/analytics/react";
 
 const Page = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/article" element={<Article />} />
-      <Route exact path="/posts/:slug" element={<Article />} />
+      <Route index element={<Home />} />
+
+      <Route exact path="/posts/:slug" element={<Articles />} />
     </Routes>
   );
 };
